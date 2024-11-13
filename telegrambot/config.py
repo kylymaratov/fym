aa1fa9ad3ebcc01e2eeb8f468244a47442c4fcc0
klm <kylymaratov@gmail.com>
2024-11-13 20:43:37 +0600

    telegrambot: edit config file

--- telegrambot/config.py
+++ telegrambot/config.py
@@ -1,3 +1,9 @@
+IS_PROD = False
+
 
 SERVER_HOST = "http://localhost:5000"
 SECRET_KEY = "1ugOG5j6Hssq6d740Bne5eMnyNtXvq1r"
+
+if IS_PROD:
+    SERVER_HOST = "https://songfiy.online"
+    SERVER_KEY = ""
