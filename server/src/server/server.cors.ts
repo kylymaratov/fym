ad21e757272854b8db51e54114a0f1656008768e
klm <kylymaratov@gmail.com>
2024-11-29 17:36:19 +0600

    server: fix

--- server/src/server/server.cors.ts
+++ server/src/server/server.cors.ts
@@ -3,11 +3,9 @@ import { serverEnv } from './server.env';
 
 const hosts = ['http://localhost:5173'];
 
-const prosHosts = ['http://localhost:3000'];
-
 export const setServerCors = (app: INestApplication) => {
   app.enableCors({
-    origin: serverEnv.isProd ? prosHosts : hosts,
+    origin: serverEnv.isProd ? "/" : hosts,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     credentials: true,
   });
